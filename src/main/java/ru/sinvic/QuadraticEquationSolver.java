ba519b80325b5090022a1421391e760977c8f9ff
Victoria <vicktoria.sin@yandex.ru>
2025-01-25 00:26:06 +0300

    add base test

--- src/main/java/ru/sinvic/QuadraticEquationSolver.java
+++ src/main/java/ru/sinvic/QuadraticEquationSolver.java
@@ -1,11 +1,42 @@
 package ru.sinvic;
 
+import java.util.stream.DoubleStream;
+
 public class QuadraticEquationSolver {
-    public int sum(int a, int b) {
-        return a + b;
+    public double[] solve(double a, double b, double c, double e) {
+        if (Math.abs(a) <= e) {
+            throw new IllegalArgumentException("a не равно 0.");
+        }
+
+        if (isContainsNaN(a, b, c) || isContainsInfinity(a, b, c)) {
+            throw new IllegalArgumentException("коэффициенты должны быть числами.");
+        }
+
+        double D = b * b - 4 * a * c;
+        if (D < -e) {
+            return new double[0];
+        }
+        double x1;
+        double x2;
+        if (D > e) {
+            x1 = (-b + Math.sqrt(D)) / (2 * a);
+            x2 = (-b - Math.sqrt(D)) / (2 * a);
+            return new double[] {x1, x2};
+        }
+
+        if (Math.abs(D) <= e) {
+            x1 = -b / (2 * a);
+            return new double[] {x1};
+        }
+        throw new UnsupportedOperationException();
     }
 
-    public int minus(int a, int b) {
-        return a - b;
+    private boolean isContainsNaN(double a, double b, double c) {
+        return DoubleStream.of(a, b, c).anyMatch(Double::isNaN);
+    }
+
+    private boolean isContainsInfinity(double a, double b, double c) {
+        return DoubleStream.of(a, b, c)
+                .anyMatch(i -> i == Double.POSITIVE_INFINITY || i == Double.NEGATIVE_INFINITY);
     }
 }
--- src/test/java/ru/sinvic/QuadraticEquationSolverTest.java
+++ src/test/java/ru/sinvic/QuadraticEquationSolverTest.java
@@ -1,20 +1,74 @@
 package ru.sinvic;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.stream.Stream;
+
+import static java.lang.Double.NaN;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 class QuadraticEquationSolverTest {
+
+    private QuadraticEquationSolver quadraticEquationSolver;
+
+    private static Stream<Arguments> generateData() {
+        return Stream.of(
+                Arguments.of(Double.POSITIVE_INFINITY, 10, 1),
+                Arguments.of(1, Double.POSITIVE_INFINITY, 1),
+                Arguments.of(1, 10, Double.POSITIVE_INFINITY),
+                Arguments.of(Double.NEGATIVE_INFINITY, 10, 1),
+                Arguments.of(1, Double.NEGATIVE_INFINITY, 1),
+                Arguments.of(1, 10, Double.NEGATIVE_INFINITY),
+                Arguments.of(NaN, 10, 1),
+                Arguments.of(1, NaN, 1),
+                Arguments.of(1, 10, NaN)
+        );
+    }
+
+    @BeforeEach
+    public void setUp() {
+        quadraticEquationSolver = new QuadraticEquationSolver();
+    }
+
+
     @Test
-    void testSum() {
-        QuadraticEquationSolver quadraticEquationSolver = new QuadraticEquationSolver();
-        assertEquals(quadraticEquationSolver.sum(2, 2), 4);
+    void testNoRoots() {
+        double[] result = quadraticEquationSolver.solve(1.0, 0, 1.0, 1e-5);
+        assertEquals(0, result.length);
     }
 
     @Test
-    void testMinus() {
-        QuadraticEquationSolver quadraticEquationSolver = new QuadraticEquationSolver();
-        assertEquals(quadraticEquationSolver.minus(3, 2), 1);
+    void testTwoRoots() {
+        double[] result = quadraticEquationSolver.solve(1.0, 0, -1.0, 1e-5);
+        Assertions.assertThat(result).containsExactlyInAnyOrder(1, -1);
     }
+
+    @Test
+    void testOneRoot() {
+        double[] result = quadraticEquationSolver.solve(2, 1e-3, 1e-8, 1e-5);
+        assertEquals(result.length, 1);
+        assertEquals(result[0], -2.5E-4);
+    }
+
+    @Test
+    void testAZero() {
+        assertThrows(IllegalArgumentException.class,
+                () -> quadraticEquationSolver.solve(1e-7, 2.0, 1.0, 1e-5));
+    }
+
+    @ParameterizedTest
+    @MethodSource("generateData")
+    public void testWrongDouble(double a, double b, double c) {
+        assertThrows(IllegalArgumentException.class,
+                () -> quadraticEquationSolver.solve(a, b, c, 1e-5));
+    }
+
+
 }
